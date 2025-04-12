4e55253d05a9139043e90e78ae2ec44d08d25781
CodeKnaap <148856216+CodeKnaap@users.noreply.github.com>
2025-04-13 00:26:58 +0200

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(
+    install_requires=['IPython.display', 'PIL', 'controlnet_aux', 'diffusers', 'gradio', 'huggingface_hub', 'io', 'java.util.', 'matplotlib.pyplot', 'numpy', 'os', 'pandas', 'random', 're', 'rembg', 'requests', 'time', 'torch', 'transformers']
+)
